5d2317e256b5399e2dc69e905fc3732ac7786874
Cracky Boy <thibault.varga@student.heaj.be>
2025-08-11 11:35:46 +0200

    debugage du fichier mood_form.php

--- mood_form.php
+++ mood_form.php
@@ -20,13 +20,13 @@
 
             <h3 class="mood_form__question">Quel est ton mood actuellement ?</h3>
 
-            <select name="mood_form__emotion" id="mood_form__emotion" class="mood_form__emotion">
+            <select name="user_emotion[]" id="mood_form__emotion" class="mood_form__emotion">
 
                 <option value="emotion"><p>Emotion</p></option>
 
             <?php foreach ($emotions as $emotion){ ?>
 
-                <option id="user_emotion" name="user_emotion[]" value="<?php echo $emotion ?>"><p><?php echo $emotion ?></p></option>
+                <option id="user_emotion" value="<?php echo $emotion ?>"><p><?php echo $emotion ?></p></option>
                 
             <?php } ?>
 
@@ -38,13 +38,13 @@
 
             <h3 class="mood_form__question">Pourquoi souhaites-tu regarder un film ?</h3>
 
-            <select name="mood_form__intention" id="mood_form__intention" class="mood_form__intention">
+            <select name="user_intention[]" id="mood_form__intention" class="mood_form__intention">
 
-                <option value="emotion"><p>Intention</p></option>
+                <option value="intention"><p>Intention</p></option>
 
             <?php foreach ($intentions as $intention){ ?>
 
-                <option id="user_intention" name="user_intention[]" value="<?php echo $intention ?>"><p><?php echo $intention ?></p></option>
+                <option id="user_intention" value="<?php echo $intention ?>"><p><?php echo $intention ?></p></option>
                 
             <?php } ?>
 
@@ -56,13 +56,13 @@
 
             <h3 class="mood_form__question">Quel style de film souhaites-tu découvrir ?</h3>
 
-            <select name="mood_form__style" id="mood_form__style" class="mood_form__style">
+            <select name="user_style[]" id="mood_form__style" class="mood_form__style">
 
-                <option value="emotion"><p>style</p></option>
+                <option value="style"><p>style</p></option>
 
             <?php foreach ($styles as $style){ ?>
 
-                <option id="user_style" name="user_style[]" value="<?php echo $style ?>"><p><?php echo $style ?></p></option>
+                <option id="user_style" value="<?php echo $style ?>"><p><?php echo $style ?></p></option>
                 
             <?php } ?>
 
